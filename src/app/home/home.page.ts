be4c85b991c585a6bd9bcf7421bf046e4bd2759e
winhackger <winhackger@gmail.com>
2019-05-04 19:47:40 +0700

    setup map done!

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, AfterContentInit, ViewChild } from '@angular/core';
+declare var google;
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit, AfterContentInit {
+  map;
+  @ViewChild('mapElement') mapElement;
+  
+  ngOnInit(): void {
+  }
+
+  
+  ngAfterContentInit(): void {
+    this.map = new google.maps.Map(
+      this.mapElement.nativeElement,
+      {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+      });
+  }
+
+}
